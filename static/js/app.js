aa68c67db408be4fc32546d1fa4615c2f7ecc81d
Bayan-daux <77173800+Bayan-daux@users.noreply.github.com>
2021-08-05 22:40:49 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+// Creating the Table
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
